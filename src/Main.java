187cae3ff01cf8c326e00f763d52c55b6ef5c8cb
Ossi <ollimatti.aikio@hotmail.com>
2018-12-07 20:23:52 +0200

    Changes for new battleship

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
-public class Main {
+
+public class main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int choice = 0;
+        System.out.println("*********************************");
+        System.out.println("*     Welcome to BATTLESHIP     *");
+        System.out.println("*    1.   Play the game         *");
+        System.out.println("*    2.    Help - Rules         *");
+        System.out.println("*    3.        Quit             *");
+        System.out.println("*********************************");
+        System.out.println("Please select one of the options (1-3): ");
+        boolean choices = true;
+        while (choices) {
+            choice = Helpers.inputInt();
+            if (choice > 3 || choice < 1) {
+                System.out.println("Invalid input! Choose one of the three options.");
+            } else {
+                choices = false;
+            }
+        }
     }
+
 }
+
+
+
+/*
+public class gameboard {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+ */
